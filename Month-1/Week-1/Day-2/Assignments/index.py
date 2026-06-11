a5ca6e89b3a479b3208e623619d320c7a4cbc0c0
mohantysuraj925-star <mohantysuraj925@gmail.com>
2026-06-11 23:29:19 +0530

    completed python day 2 arithmetic and comparison operators assignments

---
+++ Month-1/Week-1/Day-2/Assignments/index.py
@@ -0,0 +1,35 @@
+num1 = float(input())
+num2 = float(input())
+print(num1 + num2)
+
+num1 = float(input())
+num2 = float(input())
+print(num1 - num2)
+
+num1 = float(input())
+num2 = float(input())
+print(num1 * num2)
+
+num1 = float(input())
+num2 = float(input())
+print(num1 / num2)
+
+num1 = float(input())
+num2 = float(input())
+print(num1 % num2)
+
+num1 = float(input())
+num2 = float(input())
+print(num1 // num2)
+
+base = float(input())
+power = float(input())
+print(base ** power)
+
+num1 = float(input())
+num2 = float(input())
+print(num1 == num2)
+
+num1 = float(input())
+num2 = float(input())
+print(num1 != num2)
