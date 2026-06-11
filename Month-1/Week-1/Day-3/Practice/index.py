4ae01cd28079c5f41275f2454696304a13a20ad9
mohantysuraj925-star <mohantysuraj925@gmail.com>
2026-06-12 00:15:47 +0530

    updated index.py in day 3 practice

---
+++ Month-1/Week-1/Day-3/Practice/index.py
@@ -0,0 +1,64 @@
+# if (True):
+#     print("This is the OP")
+
+# a = input("Enter a number: ")
+
+# if (int(a) > 0):
+#     print("A is Greater than 0")
+# else:
+#     print("A is not Greater than 0")
+
+
+# battery=int(input("Enter the battery percentage: ")) 
+        
+# if battery == 100:
+#     print("Battery is Full")
+# elif battery <= 50:
+#     print("Battery is less than 50%")
+# elif battery < 20:
+#     print("Save your battery")   
+# else:
+#     print("Switch off your phone")           
+        
+        
+# email = input("Enter your email: ")
+# password = input("Enter your password: ") 
+
+
+# if email =="admin@admin.com":
+#         if password == "admin123":
+#             print("Login successful")
+#         else:
+#             print("Incorrect password")
+# else:
+#     print("Not a admin")                
+
+
+
+# for i in range(4):
+#     print(i)
+    
+
+# for i in range(1,4):
+#     print(i)
+    
+    
+
+# for i in range(1,25,5):
+#     print(i)
+
+
+# a = 1
+# while a <= 5:
+#     print(a)
+#     a += 1
+
+# 5 to 0
+
+# for x in range(5,0,-1):
+#     print(x)
+
+y=5
+while y > 0:
+    print(y)
+    y -= 1
