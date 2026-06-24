c20ae720efe8b55122b65834488aa5d49ff4963c
mohantysuraj925-star <mohantysuraj925@gmail.com>
2026-06-24 22:12:22 +0530

    fix deployment path in wsgi

--- Month-1/Week-3/Day-2/Practice/SuraajCore/wsgi.py
+++ Month-1/Week-3/Day-2/Practice/SuraajCore/wsgi.py
@@ -8,6 +8,8 @@ https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
 """
 
 import os
+import sys
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 from django.core.wsgi import get_wsgi_application
 
