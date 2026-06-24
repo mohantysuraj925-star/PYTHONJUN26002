376e4fc1f9033f6c0b88a648457860b9b7426dcf
mohantysuraj925-star <mohantysuraj925@gmail.com>
2026-06-24 22:43:57 +0530

    update real urls and clear paths

--- Month-1/Week-3/Day-2/Practice/SuraajCore/urls.py
+++ Month-1/Week-3/Day-2/Practice/SuraajCore/urls.py
@@ -1,11 +1,10 @@
 from django.contrib import admin
 from django.urls import path
-from KumarApp.views import welcome, post_data, handle_data
+from firstapi.views import welcome,postmethod,getorpost
 
-# URL Patterns for SuraajCore Project
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('welcome/', welcome, name="welcome"),      # GET only
-    path('post/', post_data, name="post_data"),     # POST only
-    path('process/', handle_data, name="handle_data"), # GET & POST
+    path('welcome/', welcome, name="welcome"),
+    path('post-method/', postmethod, name="postmethod"),
+    path('get-or-post/', getorpost, name="getorpost")
 ]
