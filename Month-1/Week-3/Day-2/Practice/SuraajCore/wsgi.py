85f40679c6dfc3116168236dac0c14d8393a7cf1
mohantysuraj925-star <mohantysuraj925@gmail.com>
2026-06-24 22:17:48 +0530

    force path settings for render

--- Month-1/Week-3/Day-2/Practice/SuraajCore/wsgi.py
+++ Month-1/Week-3/Day-2/Practice/SuraajCore/wsgi.py
@@ -1,18 +1,9 @@
-"""
-WSGI config for SuraajCore project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
-"""
-
 import os
 import sys
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'SuraajCore.settings')
+# Render ke liye absolute path set kar rahe hain
+sys.path.append('/opt/render/project/src/Month-1/Week-3/Day-2/Practice')
 
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'SuraajCore.settings')
 application = get_wsgi_application()
--- Month-1/Week-3/Day-2/Practice/manage.py
+++ Month-1/Week-3/Day-2/Practice/manage.py
@@ -1,11 +1,9 @@
 #!/usr/bin/env python
-"""Django's command-line utility for administrative tasks."""
 import os
 import sys
 
-
 def main():
-    """Run administrative tasks."""
+    sys.path.append('/opt/render/project/src/Month-1/Week-3/Day-2/Practice')
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'SuraajCore.settings')
     try:
         from django.core.management import execute_from_command_line
@@ -16,7 +14,6 @@ def main():
             "forget to activate a virtual environment?"
         ) from exc
     execute_from_command_line(sys.argv)
-
 
 if __name__ == '__main__':
     main()
