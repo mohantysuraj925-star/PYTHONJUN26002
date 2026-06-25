92c18850c88992d0fd910fe96f429413bd05ba70
mohantysuraj925-star <mohantysuraj925@gmail.com>
2026-06-25 12:21:57 +0530

    Fix: Add static settings and gunicorn

--- Month-1/Week-3/Day-3/Assignments/StudentProject/settings.py
+++ Month-1/Week-3/Day-3/Assignments/StudentProject/settings.py
@@ -1,3 +1,4 @@
+import os
 """
 Django settings for StudentProject project.
 
@@ -117,3 +118,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
