5c08e6ae1bb2512a0f573fd4e8605b8d924dae05
mohantysuraj925-star <mohantysuraj925@gmail.com>
2026-06-26 14:41:30 +0530

    Update student project routing configuration

--- Month-1/Week-3/Day-4/Assignments/StudentProject/urls.py
+++ Month-1/Week-3/Day-4/Assignments/StudentProject/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.student_list_or_create, name='student_list_or_create'),
     path('api/students/', views.student_list_or_create, name='student_list_or_create'),
     path('api/students/<int:pk>/', views.student_detail, name='student_detail'),
 ]
