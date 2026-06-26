c0281aab04dccb90d2bc644b2c970e440cd206dc
mohantysuraj925-star <mohantysuraj925@gmail.com>
2026-06-26 14:36:20 +0530

    Update student project logic and core files

---
+++ Month-1/Week-3/Day-4/Assignments/StudentProject/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('api/students/', views.student_list_or_create, name='student_list_or_create'),
+    path('api/students/<int:pk>/', views.student_detail, name='student_detail'),
+]
