e6625d4b0e339b1ed019889d35a59c34ddcbcf48
mohantysuraj925-star <mohantysuraj925@gmail.com>
2026-06-26 15:53:49 +0530

    Production release: Finalized pagination logic and complete standard CRUD architecture endpoints

--- Month-1/Week-3/Day-4/Assignments/StudentProject/StudentProject/urls.py
+++ Month-1/Week-3/Day-4/Assignments/StudentProject/StudentProject/urls.py
@@ -4,7 +4,6 @@ from api import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.student_list_or_create, name='student_list_or_create'),
     path('api/students/', views.student_list_or_create, name='student_list_or_create'),
     path('api/students/<int:pk>/', views.student_detail, name='student_detail'),
 ]
