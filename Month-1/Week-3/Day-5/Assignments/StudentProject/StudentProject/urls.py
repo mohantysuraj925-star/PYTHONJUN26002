d524e6d680730b4b3b87271a5133b24320c4ad39
mohantysuraj925-star <mohantysuraj925@gmail.com>
2026-06-29 16:10:35 +0530

    fix: explicit configuration mapping for live root validation

--- Month-1/Week-3/Day-5/Assignments/StudentProject/StudentProject/urls.py
+++ Month-1/Week-3/Day-5/Assignments/StudentProject/StudentProject/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import path
 from api import views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home_index, name='home_index'),
