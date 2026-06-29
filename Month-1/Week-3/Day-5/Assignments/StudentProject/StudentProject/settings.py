72511400e8281e5db2adc0c0f6d14b0a4e5e6b61
mohantysuraj925-star <mohantysuraj925@gmail.com>
2026-06-29 16:00:37 +0530

    fix: resolve admin.E403 templates declaration inside day5 configuration

--- Month-1/Week-3/Day-5/Assignments/StudentProject/StudentProject/settings.py
+++ Month-1/Week-3/Day-5/Assignments/StudentProject/StudentProject/settings.py
@@ -3,6 +3,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-day5-auth-production-safe-key'
 DEBUG = True
 ALLOWED_HOSTS = ['*']
+
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -14,6 +15,7 @@ INSTALLED_APPS = [
     'rest_framework.authtoken',
     'api',
 ]
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -23,7 +25,25 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
 ROOT_URLCONF = 'StudentProject.urls'
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
 DATABASES = {'default': {'ENGINE': 'django.db.backends.sqlite3', 'NAME': BASE_DIR / 'db.sqlite3'}}
 REST_FRAMEWORK = {'DEFAULT_AUTHENTICATION_CLASSES': ['rest_framework.authentication.TokenAuthentication']}
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
